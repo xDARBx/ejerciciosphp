8b6e1488d7fa58c7461af1e3fdc1765a14b7469d
Daniel Alberto Rocha Bartaburu <89049112+xDARBx@users.noreply.github.com>
2023-10-10 15:47:42 -0300

    Add files via upload

---
+++ UNIDAD2/ejercicio 10.js
@@ -0,0 +1,11 @@
+//10. Se le solicita al usuario que ingrese el diámetro de un círculo. Realice un programa para calcular e informar el perímetro y el área del círculo. Se considerará para este ejercicio que el valor de PI será 3.1416.
+
+
+var radio = prompt("Ingrese el diámetro de un círculo: ")
+radio = Number(radio)
+var pi = 3.1416
+var area= pi * radio
+var longitud = 2 * pi * radio
+
+
+alert("El área del círculo es: "+ area + "cm" + " y el perímetro es de: " + longitud + "cm")
