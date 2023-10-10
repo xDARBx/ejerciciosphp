ff15010fa9601e98e19629e178c659107232caad
Daniel Alberto Rocha Bartaburu <89049112+xDARBx@users.noreply.github.com>
2023-10-10 15:25:23 -0300

    Add files via upload

---
+++ UNIDAD1/ejercicio 1.js
@@ -0,0 +1,6 @@
+// 1.Realice un algoritmo que muestre el nombre y apellido de un usuario
+
+var nombre = "Jose"
+var apellido = "Zapata"
+
+alert("Mi nombre es: " + nombre+ " "+apellido+ " aka el profe cebador")
