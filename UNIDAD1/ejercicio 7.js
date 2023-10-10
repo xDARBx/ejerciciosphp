82795ea3eac597e1bcee2addae4650ddb5e7bb15
Daniel Alberto Rocha Bartaburu <89049112+xDARBx@users.noreply.github.com>
2023-10-10 15:37:24 -0300

    Add files via upload

---
+++ UNIDAD1/ejercicio 7.js
@@ -0,0 +1,7 @@
+//7. Realice el algoritmo que arme y muestre el mensaje "Bienvenido", seguido del nombre y apellido del usuario.
+
+var nombre = "Jose"
+var apellido = "Zapata"
+var saludo = "Bienvenido: "
+
+alert(saludo + nombre + " " + apellido)
