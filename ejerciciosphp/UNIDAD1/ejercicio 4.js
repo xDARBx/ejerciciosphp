ddba7f2420a065aa08c4a86306e465e3dfc451d2
Paola Guzman <108774861+paoguzmanok@users.noreply.github.com>
2023-10-13 15:36:38 -0300

    Add files via upload

---
+++ ejerciciosphp/UNIDAD1/ejercicio 4.js
@@ -0,0 +1,8 @@
+//4. Realice el algoritmo para calcular e informar el perímetro y el área del cuadrado.
+
+var l1 = 20
+var area = l1 * l1
+var calcular = l1 * 4
+
+
+alert("El área del cuadrado es de: " + area+"m" + " y su perímetro es de: " + calcular+"m2")
