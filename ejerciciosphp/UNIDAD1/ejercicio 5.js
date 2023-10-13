03122f65a8a2ef52bc8e9d7ddae09088f12a0d28
Paola Guzman <108774861+paoguzmanok@users.noreply.github.com>
2023-10-13 16:26:21 -0300

    Delete ejerciciosphp directory

--- ejerciciosphp/UNIDAD1/ejercicio 5.js
+++
@@ -1,9 +0,0 @@
-//5. Realice el algoritmo para calcular e informar el perímetro y el área del rectángulo.
-
-var base = 20
-var altura = 40
-var area = base * altura
-var perimetro = base*2 + altura*2
-
-
-alert("El área del rectángulo es de: " + area+"m" + " y su perímetro es de: " + perimetro+"m2")
--- ejerciciosphp/UNIDAD1/ejercicio 6.js
+++
@@ -1,8 +0,0 @@
-//6. Realice el algoritmo para calcular e informar el resto de la división entera entre los valores.
-
-var v1 = 55
-var v2 = 25
-var calcular = v1 % v2
-
-
-alert("El resto entre la división de: " + v1 + " y " + v2 + " es de: " + calcular)
